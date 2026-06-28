bc203c9d4469f2a563de4bbced59f1cda0906bfd
Lukas Krasauskas <lukas.krasauskas@misu.su.se>
2026-06-28 19:41:14 +0200

    Changes for generation of L1b ncdf files to make it possible to use AWS file for ctreating L1b ncdf file that has the same structure as one generated from Bolin zarr files.

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,10 +1,12 @@
 from mats_utils.rawdata.read_data import read_MATS_data, read_MATS_PM_data, load_multi_parquet
 from mats_l2_processing.parameters import make_conf
-from mats_l2_processing.io import write_ncdf_L1b
-from mats_l2_processing.util import get_filter
+from mats_l2_processing.pointing import get_deg_map, faster_heights
+from mats_l2_processing.io import write_ncdf_L1, write_ncdf_L1b_zarr_format, read_ncdf
+from mats_l2_processing.util import get_filter, multiprocess
 from mats_utils.rawdata.read_data import store_as_parquet
-from mats_utils.rawdata.release import write_ncdf_L1b_release
+# from mats_utils.rawdata.release import write_ncdf_L1b_release
 import argparse
+import numpy as np
 import datetime as DT
 
 
@@ -18,16 +20,31 @@ def get_args():
     parser.add_argument("--version", dest="VERSION", type=str,
                         help="Data version.")
     parser.add_argument("--channel", dest="CHANNEL", type=str, help="Channel to retrieve",
-                        choices=["IR1", "IR2", "IR3", "IR4", "UV1", "UV2", "all", "PM"])
+                        choices=["IR1", "IR2", "IR3", "IR4", "UV1", "UV2", "all", "PM", "NADIR"])
+    parser.add_argument("--filter", dest="FILTER", type=str,
+                        help="Data filters. Syntax: field_name1:min:max,field_name2...")
     parser.add_argument("--from_parquet_dir", type=str,
                         help="Get data from local parquet files from specifief dir., not AWS.")
     parser.add_argument("--conf", type=str, help="Read configuration from file.")
     parser.add_argument("--ncdf_out", type=str, help="Output file name (ncdf)")
+    parser.add_argument("--level", type=str, choices=["1a", "1b"], default="1b", help="Data level.")
+    parser.add_argument("--ncdf_format", type=str, choices=["zarr_like", "all_vars"], default="zarr_like",
+                        help="ncdf format for writing leNcdf format for writing level 1b data.")
     parser.add_argument("--release", type=str,
-                        help="Make a release-type ncdf with the specified release eersion.")
+                        help="Make a release-type ncdf with the specified release version.")
     parser.add_argument("--parquet_out", type=str, help="Output file name (parquet)")
+
     parser.add_argument("--meta", action="store_true", help="Do not store images themselves, just metadata.")
+    parser.add_argument("-t", action="store_true", help="Calculate tangent point coordinates for every pixel.")
+    parser.add_argument("--dist_data", type=str, default="/home/lk/L2_data/distortion_splines_tck.nc",
+                        help="A ncdf file with distortion data")
+    parser.add_argument("--processes", type=int, default=15, help="Number of processes for TP calculations.")
     return parser.parse_args()
+
+
+def tp_calc_wrapper(image, common_args):
+    image["time"] = image["EXPDate"].to_pydatetime()
+    return faster_heights(image, None, full_coords=True, deg_map=common_args[0])
 
 
 def main():
@@ -42,19 +59,51 @@ def main():
             dftop = read_MATS_PM_data(DT.datetime(*conf.START_TIME), DT.datetime(*conf.STOP_TIME),
                                       {}, level="1a", version=conf.VERSION)
         else:
+            filt = get_filter(conf.CHANNEL)
+            if args.FILTER is not None:
+                for f in args.FILTER.split(","):
+                    items = f.split(':')
+                    assert len(items) == 3, "Malformed filter parameter!"
+                    filt[items[0]] = [float(items[1]), float(items[2])]
+
             dftop = read_MATS_data(DT.datetime(*conf.START_TIME), DT.datetime(*conf.STOP_TIME),
-                                   get_filter(conf.CHANNEL), level="1b", version=conf.VERSION)
+                                   filt, level=args.level, version=conf.VERSION)
     # breakpoint()
-    if args.ncdf_out:
-        if args.release:
-            write_ncdf_L1b_release(dftop, args.ncdf_out, conf.CHANNEL, conf.VERSION, args.release)
-        else:
-            write_ncdf_L1b(dftop, args.ncdf_out, conf.CHANNEL, conf.VERSION, im_calibrated=not args.meta)
+
     if args.parquet_out:
         store_as_parquet(dftop, f"{args.parquet_out}.parquet.gzip")
         # dftop.to_parquet(f"{args.parquet_out}.parquet.gzip", compression='gzip', engine='fastparquet')
-    elif not args.ncdf_out:
-        raise RuntimeError("Please specify output file name for at least one format (ncdf and/or parquet).")
+    else:
+        # if args.release:
+        #     write_ncdf_L1b_release(dftop, args.ncdf_out, conf.CHANNEL, conf.VERSION, args.release)
+        # else:
+        #     write_ncdf_L1(dftop, args.ncdf_out, conf.CHANNEL, conf.VERSION, image=not args.meta, level=args.level)
+        if args.ncdf_out is None:
+            ofile = f"L1b_{conf.CHANNEL}"
+            if args.START_TIME:
+                dates = ["_".join([str(x) for x in array]) for array in [args.START_TIME, args.STOP_TIME]]
+                ofile += f"-{'-'.join(dates)}"
+            ofile = f"{ofile}.nc"
+        else:
+            ofile = args.ncdf_out
+
+        if args.ncdf_format == "all_vars" or args.level == "1a":
+            write_ncdf_L1(dftop, ofile, conf.CHANNEL, args.VERSION, image=not args.meta, level=args.level)
+        else:
+            if args.t:
+                variables = ["EXPDate", "afsAttitudeState", "afsGnssStateJ2000", "NCSKIP", "NRSKIP", "NCOL", "NROW",
+                             "NRBIN", "NCBINCCDColumns", "channel", "TEXPMS", "qprime"]
+                dist_data = read_ncdf(args.dist_data, None, get_units=False)
+                deg_map = get_deg_map(dftop.iloc[0], dist_data)
+                tp_data = multiprocess(tp_calc_wrapper, dftop, variables, args.processes, [deg_map])
+                num_img = len(tp_data)
+                tp_vars = {name: np.stack([tp_data[k][name] for k in range(num_img)], axis=0)
+                           for name in ["TPECEFx", "TPECEFy", "TPECEFz", "TPheightPixel"]}
+                del tp_data
+            else:
+                tp_vars = None
+
+            write_ncdf_L1b_zarr_format(dftop, ofile, conf.CHANNEL, args.VERSION, extra_vars=tp_vars)
 
 
 if __name__ == "__main__":
